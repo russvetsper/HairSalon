ec62710604e58fb761a2977045f65b263b66f11e
russvetsper <russvetsper@gmail.com>
2016-08-10 22:02:23 -0700

    add setters and getters for client

---
+++ Objects/Client.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Client
+  {
+    private int _id;
+    private string _name;
+    private int _stylistId;
+
+    public Client(string Name, int StylistId, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _stylistId = StylistId;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public int GetStylistId()
+     {
+       return _stylistId;
+     }
+     public void SetStylistId(int newStylistId)
+     {
+       _stylistId = newStylistId;
+     }
+  }
+}
