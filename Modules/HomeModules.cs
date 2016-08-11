438ef93c141086b8795de25431dae5eccf816238
russvetsper <russvetsper@gmail.com>
2016-08-10 21:33:55 -0700

    add index page and first homemodule page

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace HairSalon
+{
+  public class Homemodule : NancyModule
+  {
+    public Homemodule()
+    {
+      Get["/"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["index.cshtml", AllStylists];
+      };
+    }
+  }
+}      
