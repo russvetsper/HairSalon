707b703bf7065e4c8328a72419f8a3cd1ca58478
russvetsper <russvetsper@gmail.com>
2016-08-10 21:02:36 -0700

    pass test1 get all

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -28,6 +28,51 @@ namespace HairSalon
       _name = newName;
     }
 
+    public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool idEquality = (this.GetId() == newStylist.GetId());
+        bool nameEquality = (this.GetName() == newStylist.GetName());
+        return (idEquality && nameEquality);
+      }
+    }
+
+    public static List<Stylist> GetAll()
+  {
+    List<Stylist> allStylists = new List<Stylist>{};
+
+    SqlConnection conn = DB.Connection();
+    conn.Open();
+
+    SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+    SqlDataReader rdr = cmd.ExecuteReader();
+
+    while(rdr.Read())
+    {
+      int stylistId = rdr.GetInt32(0);
+      string stylistName = rdr.GetString(1);
+      Stylist newStylist = new Stylist(stylistName, stylistId);
+      allStylists.Add(newStylist);
+    }
+
+    if (rdr != null)
+    {
+      rdr.Close();
+    }
+    if (conn != null)
+    {
+      conn.Close();
+    }
+
+    return allStylists;
+  }
+
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
@@ -35,6 +80,6 @@ namespace HairSalon
       SqlCommand cmd = new SqlCommand ("DELETE FROM stylists;",conn);
       cmd.ExecuteNonQuery();
       conn.Close();
-    }  
+    }
   }
 }
--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -13,10 +13,21 @@ namespace HairSalon
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
 
+    [Fact]
+  public void Test1_EmptyDatabase()
+  {
+    //Arrange, Act
+    int result = Stylist.GetAll().Count;
+
+    //Assert
+    Assert.Equal(0, result);
+  }
+
+
   public void Dispose()
     {
       Stylist.DeleteAll();
-      
+
     }
   }
 }
