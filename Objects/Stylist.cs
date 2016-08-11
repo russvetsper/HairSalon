3f588a67444159f4872b78a3b5b0f43cd8f003a9
russvetsper <russvetsper@gmail.com>
2016-08-10 20:47:00 -0700

    add stylist setters and getters

---
+++ Objects/Stylist.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _name;
+
+    public Stylist(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+  }
+}    
